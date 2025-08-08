428df89335e516b25e413281c04d41b9d3c51a42
Rubikal1 <maxeguz@gmail.com>
2025-08-08 03:02:34 -0400

    New image
    New image

--- utils/imageAssets.js
+++ utils/imageAssets.js
@@ -1,4 +1,4 @@
 module.exports = {
-  TICKET_BANNER_URL: 'https://imgur.com/a/u2D7NDZ',
+  TICKET_BANNER_URL: 'https://cdn3.mapstr.gg/7883cc423c654f52f97e44141a610284.png',
   ICON_URL: 'https://i.imgur.com/J8jWU7t.gif',
 };
