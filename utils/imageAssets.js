f6e40ae5e99302f71500a42c5032d7a070d4eb0a
Rubikal1 <maxeguz@gmail.com>
2025-09-04 03:22:54 -0400

    image

--- utils/imageAssets.js
+++ utils/imageAssets.js
@@ -1,4 +1,4 @@
 module.exports = {
   TICKET_BANNER_URL: 'https://cdn3.mapstr.gg/6b5c747ff2082baa0b802df8748f36bc.gif',
-  ICON_URL: 'https://cdn3.mapstr.gg/6b5c747ff2082baa0b802df8748f36bc.gif',
+  ICON_URL: 'https://cdn3.mapstr.gg/cefd5b5b46e1fd7b8797472a36e65d8a.gif',
 };
