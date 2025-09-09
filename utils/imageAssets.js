3de67a8097cc23e6dfc3cff9f56b378ec4decb9e
Rubikal1 <maxeguz@gmail.com>
2025-09-09 19:28:58 -0400

    Update imageAssets.js

--- utils/imageAssets.js
+++ utils/imageAssets.js
@@ -1,4 +1,4 @@
 module.exports = {
-  TICKET_BANNER_URL: 'https://cdn3.mapstr.gg/6b5c747ff2082baa0b802df8748f36bc.gif',
-  ICON_URL: 'https://cdn3.mapstr.gg/cefd5b5b46e1fd7b8797472a36e65d8a.gif',
+  TICKET_BANNER_URL: 'https://cdn3.mapstr.gg/a69240c2f47fa6243ced2bc468b47be7.png',
+  ICON_URL: 'https://cdn3.mapstr.gg/07fbafb53502931e3416fe9a8b8b734d.png',
 };
